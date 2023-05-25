3da54f3573e2ed5976deebcfbbd3bcf5b65a3a4e
yuto ichinose <yuto.0526.sano@gmail.com>
2023-05-25 15:14:09 +0900

    first commit

---
+++ src/test.ts
@@ -0,0 +1,35 @@
+import { Configuration, OpenAIApi } from "openai";
+import { generateExample } from "./lib/createExample";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const apiKey = process.env.OPENAI_API_KEY;
+const wordLang = "英語";
+const wordName = "hello";
+const wordMean = "こんにちわ";
+
+const main = async () => {
+  try {
+    const configuration = new Configuration({
+      apiKey: apiKey,
+    });
+    const openai = new OpenAIApi(configuration);
+    console.log("作成中");
+    const result = await generateExample(
+      `${wordLang}言語の${wordName}という${wordMean}という意味の単語を用いて簡単な例文を作成して`,
+      "gpt-3.5-turbo",
+      "user",
+      openai
+    ); // ChatGPTから例文を生成
+    if (result.success) {
+      console.log(result.content);
+      console.log("作成完了");
+    } else {
+      console.log(result.content);
+    }
+  } catch (error) {
+    console.error("Failed to generate example:", error);
+  }
+};
+
+main();
